a393af392596a60ab3d57973f063c0c0a5639f62
z2++ <h.nagato@gmail.com>
2012-01-05 23:18:17 +0900

    Initial commit.

---
+++ paste-to-automator.rb
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+
+=begin
+$ # setup
+$ defaults write com.apple.screencapture location ~/Dropbox/Public
+$ killall SystemUIServer
+=end
+
+YOUR_DROPBOX_ID = 0000000
+
+
+require 'fileutils'
+require 'digest/md5'
+
+include FileUtils
+
+links = (ARGV || []).map {|p|
+  dirname  = File.dirname p
+  filename = File.basename p
+  extname  = File.extname p
+
+  filename = "#{Digest::MD5.hexdigest(p)}#{extname}"
+
+  mv p, "#{dirname}/#{filename}"
+
+  "http://dl.dropbox.com/u/#{YOUR_DROPBOX_ID}/#{filename}"
+}.join('\n')
+
+
+IO.popen('pbcopy', 'w').print links
+
+begin
+  require 'rubygems'
+  require 'notify'
+  Notify.notify('Copied!', links)
+rescue
+  system "which growlnotify >/dev/null 2>&1 && growlnotify -m #{links} -t Copied!"
+end
+
